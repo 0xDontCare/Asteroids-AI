2a8f860936eb2ad99685b7b860d6bd7247e106f8
0xDontCare <hogar1.veliki@gmail.com>
2023-12-10 23:06:21 +0100

    Added smaller common utility module

---
+++ include/commonUtility.h
@@ -0,0 +1,43 @@
+/**
+ * @file commonUtility.h
+ * @author 0xDontCare (you@domain.com)
+ * @brief Common utility functions for all three programs (manager, game and network). All functions have prefix `cu_`.
+ * @version 0.2
+ * @date 04.12.2023.
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef COMMONUTILITY_H
+#define COMMONUTILITY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * @brief Check if given C string contains only alphanumeric characters.
+ * 
+ * @param string Pointer to null-terminated string. 
+ * @return 1 if string is alphanumeric, 0 otherwise.
+ * 
+ * @note If string is NULL, function returns 1 as if it passed the test.
+ */
+int cu_CStringIsAlphanumeric(char *string);
+
+/**
+ * @brief Check if given C string contains only numeric characters.
+ * 
+ * @param string Pointer to null-terminated string. 
+ * @return 1 if string is numeric, 0 otherwise.
+ * 
+ * @note If string is NULL, function returns 0 as if it failed the test.
+ */
+int cu_CStringIsNumeric(char *string);
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif  // COMMONUTILITY_H
