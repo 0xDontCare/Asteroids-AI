797717bc5f3c5ad917daaf85f3a767da0dd20f36
0xDontCare <hogar1.veliki@gmail.com>
2023-12-23 21:12:32 +0100

    Placeholder neural network program

---
+++ neurons/src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>   // input/output
+#include <stdlib.h>  // malloc, free, etc.
+
+#include "sharedMemory.h"  // shared memory
+#include "xLinear.h"       // matrix operations
+#include "xString.h"       // string operations (for parsing command line arguments)
+
+int main(int argc, char *argv[]) {
+    // TODO: parse command line arguments
+
+    // TODO: connect to shared memory from parsed arguments
+
+    // TODO: initialize matrices for network (load from file or generate random)
+
+    // TODO: start network execution loop
+
+    // TODO: free memory and close shared memory at program exit
+
+    return 0;
+}
