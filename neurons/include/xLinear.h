62a53060652e0063947b5be9f528d33693e6d9da
--get <hogar1.veliki@gmail.com>
2024-05-09 00:59:24 +0200

    Refactored xLinear module for readability and consistency with the rest of the codebase

--- neurons/include/xLinear.h
+++ neurons/include/xLinear.h
@@ -5,7 +5,7 @@
  * @version 0.5
  * @date 05.03.2024.
  *
- * @copyright All rights reserved (c) 2023
+ * @copyright All rights reserved (c) 2024
  *
  */
 
@@ -15,217 +15,209 @@
 #include <stdint.h>
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-  /**
-   * @brief Matrix structure.
-   *
-   */
-  typedef struct
-  {
+typedef struct {
     uint32_t rows;  // number of rows
     uint32_t cols;  // number of columns
     float *data;    // matrix data
-  } xMatrix;
+} xMatrix;
 
-  // constructor/destructor
-  // ----------------------------------------------------------------------------------------------
+// constructor/destructor
+// ----------------------------------------------------------------------------------------------
 
-  /**
-   * @brief Create new empty matrix.
-   *
-   * @param rows Number of rows.
-   * @param cols Number of columns.
-   * @return Pointer to allocated matrix. NULL if allocation fails or invalid dimensions.
-   */
-  xMatrix *xMatrix_new(uint32_t rows, uint32_t cols);
+/**
+ * @brief Create new empty matrix.
+ *
+ * @param rows Number of rows.
+ * @param cols Number of columns.
+ * @return Pointer to allocated matrix. NULL if allocation fails or invalid
+ * dimensions.
+ */
+xMatrix *xMatrix_new(uint32_t rows, uint32_t cols);
 
-  /**
-   * @brief Free matrix from memory.
-   *
-   * @param mat Pointer to matrix.
-   *
-   * @note Make sure to remove dangling pointers to freed matrix.
-   */
-  void xMatrix_free(xMatrix *mat);
+/**
+ * @brief Free matrix from memory.
+ *
+ * @param mat Pointer to matrix.
+ *
+ * @note Make sure to remove dangling pointers to freed matrix.
+ */
+void xMatrix_free(xMatrix *mat);
 
-  // instantiating functions (allocates memory for returned values)
-  // ----------------------------------------------------------------------------------------------
+// instantiating functions (allocates memory for returned values)
+// ----------------------------------------------------------------------------------------------
 
-  /**
-   * @brief Create new matrix from selected range in another matrix.
-   *
-   * @param mat Pointer to matrix.
-   * @param row_start Starting index of slice row (inclusive).
-   * @param row_end Ending index of slice row (exclusive, i.e. not included in slice).
-   * @param col_start Starting index of slice column (inclusive).
-   * @param col_end Ending index of slice column (exclusive, i.e. not included in slice).
-   * @return Pointer to matrix slice. NULL if allocation fails or invalid indices.
-   */
-  xMatrix *xMatrix_slice(xMatrix *mat, uint32_t row_start, uint32_t row_end, uint32_t col_start,
-                         uint32_t col_end);
+/**
+ * @brief Create new matrix from selected range in another matrix.
+ *
+ * @param mat Pointer to matrix.
+ * @param row_start Starting index of slice row (inclusive).
+ * @param row_end Ending index of slice row (exclusive, i.e. not included in slice).
+ * @param col_start Starting index of slice column (inclusive).
+ * @param col_end Ending index of slice column (exclusive, i.e. not included in slice).
+ * @return Pointer to matrix slice. NULL if allocation fails or invalid indices.
+ */
+xMatrix *xMatrix_slice(xMatrix *mat, uint32_t row_start, uint32_t row_end, uint32_t col_start, uint32_t col_end);
 
-  /**
-   * @brief Create identity matrix of given dimension.
-   *
-   * @param dim Dimension of identity matrix.
-   * @return Pointer to identity matrix. NULL if allocation fails or invalid dimension.
-   */
-  xMatrix *xMatrix_identity(uint32_t dim);
+/**
+ * @brief Create identity matrix of given dimension.
+ *
+ * @param dim Dimension of identity matrix.
+ * @return Pointer to identity matrix. NULL if allocation fails or invalid
+ * dimension.
+ */
+xMatrix *xMatrix_identity(uint32_t dim);
 
-  /**
-   * @brief Extract row from matrix into new matrix.
-   *
-   * @param mat Pointer to matrix.
-   * @param rowIndex Index of row to extract.
-   * @return Pointer to row matrix. NULL if allocation fails or invalid index.
-   */
-  xMatrix *xMatrix_row(xMatrix *mat, uint32_t rowIndex);
+/**
+ * @brief Extract row from matrix into new matrix.
+ *
+ * @param mat Pointer to matrix.
+ * @param rowIndex Index of row to extract.
+ * @return Pointer to row matrix. NULL if allocation fails or invalid index.
+ */
+xMatrix *xMatrix_row(xMatrix *mat, uint32_t rowIndex);
 
-  /**
-   * @brief Extract column from matrix into new matrix.
-   *
-   * @param mat Pointer to matrix.
-   * @param colIndex Index of column to extract.
-   * @return Pointer to column matrix. NULL if allocation fails or invalid index.
-   */
-  xMatrix *xMatrix_col(xMatrix *mat, uint32_t colIndex);
+/**
+ * @brief Extract column from matrix into new matrix.
+ *
+ * @param mat Pointer to matrix.
+ * @param colIndex Index of column to extract.
+ * @return Pointer to column matrix. NULL if allocation fails or invalid index.
+ */
+xMatrix *xMatrix_col(xMatrix *mat, uint32_t colIndex);
 
-  // non-instantiating functions (does not allocate memory for returned values)
-  // ----------------------------------------------------------------------------------------------
+// non-instantiating functions (does not allocate memory for returned values)
+// ----------------------------------------------------------------------------------------------
 
-  /**
-   * @brief Add two matrices and store result in third matrix.
-   *
-   * @param res Pointer to result matrix.
-   * @param mat1 Pointer to first matrix.
-   * @param mat2 Pointer to second matrix.
-   *
-   * @note Result matrix must be of same size as input matrices.
-   *
-   * @note Does nothing if any matrix is NULL or dimensions are invalid.
-   */
-  void xMatrix_add(xMatrix *res, xMatrix *mat1, xMatrix *mat2);
+/**
+ * @brief Add two matrices and store result in third matrix.
+ *
+ * @param res Pointer to result matrix.
+ * @param mat1 Pointer to first matrix.
+ * @param mat2 Pointer to second matrix.
+ *
+ * @note Result matrix must be of same size as input matrices.
+ *
+ * @note Does nothing if any matrix is NULL or dimensions are invalid.
+ */
+void xMatrix_add(xMatrix *res, xMatrix *mat1, xMatrix *mat2);
 
-  /**
-   * @brief Subtract two matrices and store result in third matrix.
-   *
-   * @param res Pointer to result matrix.
-   * @param mat1 Pointer to first matrix.
-   * @param mat2 Pointer to second matrix.
-   *
-   * @note Result matrix must be of same size as input matrices.
-   *
-   * @note Does nothing if any matrix is NULL or dimensions are invalid.
-   */
-  void xMatrix_sub(xMatrix *res, xMatrix *mat1, xMatrix *mat2);
+/**
+ * @brief Subtract two matrices and store result in third matrix.
+ *
+ * @param res Pointer to result matrix.
+ * @param mat1 Pointer to first matrix.
+ * @param mat2 Pointer to second matrix.
+ *
+ * @note Result matrix must be of same size as input matrices.
+ *
+ * @note Does nothing if any matrix is NULL or dimensions are invalid.
+ */
+void xMatrix_sub(xMatrix *res, xMatrix *mat1, xMatrix *mat2);
 
-  /**
-   * @brief Multiply two matrices and store result in third matrix.
-   *
-   * @param res Pointer to result matrix.
-   * @param mat1 Pointer to first matrix.
-   * @param mat2 Pointer to second matrix.
-   *
-   * @note Result matrix must be of size (mat1->rows x mat2->cols).
-   *
-   * @warning Result matrix can not have same pointer as input matrices (due to in-place
-   * operations).
-   *
-   * @note Does nothing if any matrix is NULL or dimensions are invalid.
-   */
-  void xMatrix_dot(xMatrix *res, xMatrix *mat1, xMatrix *mat2);
+/**
+ * @brief Multiply two matrices and store result in third matrix.
+ *
+ * @param res Pointer to result matrix.
+ * @param mat1 Pointer to first matrix.
+ * @param mat2 Pointer to second matrix.
+ *
+ * @note Result matrix must be of size (mat1->rows x mat2->cols).
+ *
+ * @warning Result matrix can not have same pointer as input matrices (due to in-place operations).
+ *
+ * @note Does nothing if any matrix is NULL or dimensions are invalid.
+ */
+void xMatrix_dot(xMatrix *res, xMatrix *mat1, xMatrix *mat2);
 
-  /**
-   * @brief Transpose matrix in-place.
-   *
-   * @param mat Pointer to matrix.
-   *
-   * @note Does nothing if matrix is NULL.
-   */
-  void xMatrix_transpose(xMatrix *mat);
+/**
+ * @brief Transpose matrix in-place.
+ *
+ * @param mat Pointer to matrix.
+ *
+ * @note Does nothing if matrix is NULL.
+ */
+void xMatrix_transpose(xMatrix *mat);
 
-  /**
-   * @brief Scale matrix by scalar in-place.
-   *
-   * @param mat Pointer to matrix.
-   * @param scale Scalar value to scale matrix by.
-   *
-   * @note Does nothing if matrix is NULL.
-   */
-  void xMatrix_scale(xMatrix *mat, float scale);
+/**
+ * @brief Scale matrix by scalar in-place.
+ *
+ * @param mat Pointer to matrix.
+ * @param scale Scalar value to scale matrix by.
+ *
+ * @note Does nothing if matrix is NULL.
+ */
+void xMatrix_scale(xMatrix *mat, float scale);
 
-  /**
-   * @brief Add two matrix rows and store result in first row.
-   *
-   * @param mat Pointer to matrix.
-   * @param row1 Index of first row.
-   * @param row2 Index of second row.
-   * @param scale Scalar value to scale second row by before adding to first row (does not affect
-   * second row values).
-   *
-   * @note Does nothing if index is out of bounds or matrix is NULL.
-   */
-  void xMatrix_rowAdd(xMatrix *mat, uint32_t row1, uint32_t row2, float scale);
+/**
+ * @brief Add two matrix rows and store result in first row.
+ *
+ * @param mat Pointer to matrix.
+ * @param row1 Index of first row.
+ * @param row2 Index of second row.
+ * @param scale Scalar value to scale second row by before adding to first row (does not affect second row values).
+ *
+ * @note Does nothing if index is out of bounds or matrix is NULL.
+ */
+void xMatrix_rowAdd(xMatrix *mat, uint32_t row1, uint32_t row2, float scale);
 
-  /**
-   * @brief Add two matrix columns and store result in first column.
-   *
-   * @param mat Pointer to matrix.
-   * @param col1 Index of first column.
-   * @param col2 Index of second column.
-   * @param scale Scalar value to scale second column by before adding to first column (does not
-   * affect second column values).
-   *
-   * @note Does nothing if index is out of bounds or matrix is NULL.
-   */
-  void xMatrix_colAdd(xMatrix *mat, uint32_t col1, uint32_t col2, float scale);
+/**
+ * @brief Add two matrix columns and store result in first column.
+ *
+ * @param mat Pointer to matrix.
+ * @param col1 Index of first column.
+ * @param col2 Index of second column.
+ * @param scale Scalar value to scale second column by before adding to first column (does not affect second column values).
+ *
+ * @note Does nothing if index is out of bounds or matrix is NULL.
+ */
+void xMatrix_colAdd(xMatrix *mat, uint32_t col1, uint32_t col2, float scale);
 
-  /**
-   * @brief Scale matrix row by scalar in-place.
-   *
-   * @param mat Pointer to matrix.
-   * @param rowIndex Index of row.
-   * @param scale Scalar value to scale row by.
-   *
-   * @note Does nothing if index is out of bounds or matrix is NULL.
-   */
-  void xMatrix_rowScale(xMatrix *mat, uint32_t rowIndex, float scale);
+/**
+ * @brief Scale matrix row by scalar in-place.
+ *
+ * @param mat Pointer to matrix.
+ * @param rowIndex Index of row.
+ * @param scale Scalar value to scale row by.
+ *
+ * @note Does nothing if index is out of bounds or matrix is NULL.
+ */
+void xMatrix_rowScale(xMatrix *mat, uint32_t rowIndex, float scale);
 
-  /**
-   * @brief Scale matrix column by scalar in-place.
-   *
-   * @param mat Pointer to matrix.
-   * @param colIndex Index of column.
-   * @param scale Scalar value to scale column by.
-   *
-   * @note Does nothing if index is out of bounds or matrix is NULL.
-   */
-  void xMatrix_colScale(xMatrix *mat, uint32_t colIndex, float scale);
+/**
+ * @brief Scale matrix column by scalar in-place.
+ *
+ * @param mat Pointer to matrix.
+ * @param colIndex Index of column.
+ * @param scale Scalar value to scale column by.
+ *
+ * @note Does nothing if index is out of bounds or matrix is NULL.
+ */
+void xMatrix_colScale(xMatrix *mat, uint32_t colIndex, float scale);
 
-  /**
-   * @brief Get value from matrix at specified index.
-   *
-   * @param mat Pointer to matrix.
-   * @param rowIndex Row index (0-based).
-   * @param colIndex Column index (0-based).
-   * @return Value at specified index. 0 if index is out of bounds or matrix is NULL.
-   */
-  float xMatrix_get(xMatrix *mat, uint32_t rowIndex, uint32_t colIndex);
+/**
+ * @brief Get value from matrix at specified index.
+ *
+ * @param mat Pointer to matrix.
+ * @param rowIndex Row index (0-based).
+ * @param colIndex Column index (0-based).
+ * @return Value at specified index. 0 if index is out of bounds or matrix is NULL.
+ */
+float xMatrix_get(xMatrix *mat, uint32_t rowIndex, uint32_t colIndex);
 
-  /**
-   * @brief Set value in matrix at specified index.
-   *
-   * @param mat Pointer to matrix.
-   * @param rowIndex Row index (0-based).
-   * @param colIndex Column index (0-based).
-   * @param value Value to set at specified index.
-   *
-   * @note Does nothing if index is out of bounds or matrix is NULL.
-   */
-  void xMatrix_set(xMatrix *mat, uint32_t rowIndex, uint32_t colIndex, float value);
+/**
+ * @brief Set value in matrix at specified index.
+ *
+ * @param mat Pointer to matrix.
+ * @param rowIndex Row index (0-based).
+ * @param colIndex Column index (0-based).
+ * @param value Value to set at specified index.
+ *
+ * @note Does nothing if index is out of bounds or matrix is NULL.
+ */
+void xMatrix_set(xMatrix *mat, uint32_t rowIndex, uint32_t colIndex, float value);
 
 #ifdef __cplusplus
 }
--- neurons/src/xLinear.c
+++ neurons/src/xLinear.c
@@ -5,385 +5,333 @@
 
 xMatrix *xMatrix_new(uint32_t rows, uint32_t cols)
 {
-  // dimension checking
-  if (rows == 0 || cols == 0 || rows > UINT32_MAX / cols || cols > UINT32_MAX / rows)
-  {
-    return NULL;
-  }
+    // dimension checking
+    if (rows == 0 || cols == 0 || rows > UINT32_MAX / cols || cols > UINT32_MAX / rows) {
+        return NULL;
+    }
 
-  // matrix allocation
-  xMatrix *mat = (xMatrix *)malloc(sizeof(xMatrix));
-  if (mat == NULL)
-  {
-    return NULL;
-  }
+    // matrix allocation
+    xMatrix *mat = (xMatrix *)malloc(sizeof(xMatrix));
+    if (mat == NULL) {
+        return NULL;
+    }
 
-  // data allocation
-  mat->data = (float *)calloc(rows * cols, sizeof(float));
-  if (mat->data == NULL)
-  {
-    free(mat);
-    return NULL;
-  }
+    // data allocation
+    mat->data = (float *)calloc(rows * cols, sizeof(float));
+    if (mat->data == NULL) {
+        free(mat);
+        return NULL;
+    }
 
-  // matrix initialization
-  mat->rows = rows;
-  mat->cols = cols;
+    // matrix initialization
+    mat->rows = rows;
+    mat->cols = cols;
 
-  return mat;
+    return mat;
 }
 
 void xMatrix_free(xMatrix *mat)
 {
-  free(mat->data);
-  free(mat);
+    free(mat->data);
+    free(mat);
 }
 
-xMatrix *xMatrix_slice(xMatrix *mat, uint32_t row_start, uint32_t row_end, uint32_t col_start,
-                       uint32_t col_end)
+xMatrix *xMatrix_slice(xMatrix *mat, uint32_t row_start, uint32_t row_end, uint32_t col_start, uint32_t col_end)
 {
-  // bound checking
-  if (row_start >= row_end || col_start >= col_end)
-  {
-    return NULL;
-  }
-
-  // matrix allocation
-  xMatrix *res = xMatrix_new(row_end - row_start, col_end - col_start);
-  if (res == NULL)
-  {
-    return NULL;
-  }
-
-  // value copy
-  for (uint32_t i = row_start; i < row_end; i++)
-  {
-    for (uint32_t j = col_start; j < col_end; j++)
-    {
-      xMatrix_set(res, i - row_start, j - col_start, xMatrix_get(mat, i, j));
+    // bound checking
+    if (row_start >= row_end || col_start >= col_end) {
+        return NULL;
     }
-  }
 
-  return res;
+    // matrix allocation
+    xMatrix *res = xMatrix_new(row_end - row_start, col_end - col_start);
+    if (res == NULL) {
+        return NULL;
+    }
+
+    // value copy
+    for (uint32_t i = row_start; i < row_end; i++) {
+        for (uint32_t j = col_start; j < col_end; j++) {
+            xMatrix_set(res, i - row_start, j - col_start, xMatrix_get(mat, i, j));
+        }
+    }
+
+    return res;
 }
 
 xMatrix *xMatrix_identity(uint32_t dim)
 {
-  // matrix allocation
-  xMatrix *res = xMatrix_new(dim, dim);
-  if (res == NULL)
-  {
-    return NULL;
-  }
+    // matrix allocation
+    xMatrix *res = xMatrix_new(dim, dim);
+    if (res == NULL) {
+        return NULL;
+    }
 
-  // value initialization
-  for (uint32_t i = 0; i < dim; i++)
-  {
-    xMatrix_set(res, i, i, 1.0f);
-  }
+    // value initialization
+    for (uint32_t i = 0; i < dim; i++) {
+        xMatrix_set(res, i, i, 1.0f);
+    }
 
-  return res;
+    return res;
 }
 
 xMatrix *xMatrix_row(xMatrix *mat, uint32_t rowIndex)
 {
-  // matrix allocation
-  xMatrix *res = xMatrix_new(1, mat->cols);
-  if (res == NULL)
-  {
-    return NULL;
-  }
+    // matrix allocation
+    xMatrix *res = xMatrix_new(1, mat->cols);
+    if (res == NULL) {
+        return NULL;
+    }
 
-  // value copy
-  for (uint32_t i = 0; i < mat->cols; i++)
-  {
-    xMatrix_set(res, 0, i, xMatrix_get(mat, rowIndex, i));
-  }
+    // value copy
+    for (uint32_t i = 0; i < mat->cols; i++) {
+        xMatrix_set(res, 0, i, xMatrix_get(mat, rowIndex, i));
+    }
 
-  return res;
+    return res;
 }
 
 xMatrix *xMatrix_col(xMatrix *mat, uint32_t colIndex)
 {
-  // matrix allocation
-  xMatrix *res = xMatrix_new(mat->rows, 1);
-  if (res == NULL)
-  {
-    return NULL;
-  }
+    // matrix allocation
+    xMatrix *res = xMatrix_new(mat->rows, 1);
+    if (res == NULL) {
+        return NULL;
+    }
 
-  // value copy
-  for (uint32_t i = 0; i < mat->rows; i++)
-  {
-    xMatrix_set(res, i, 0, xMatrix_get(mat, i, colIndex));
-  }
+    // value copy
+    for (uint32_t i = 0; i < mat->rows; i++) {
+        xMatrix_set(res, i, 0, xMatrix_get(mat, i, colIndex));
+    }
 
-  return res;
+    return res;
 }
 
 void xMatrix_add(xMatrix *res, xMatrix *mat1, xMatrix *mat2)
 {
-  // pointer checking
-  if (res == NULL || mat1 == NULL || mat2 == NULL)
-  {
-    return;
-  }
-
-  // dimension checking
-  if (res->rows != mat1->rows || res->rows != mat2->rows || res->cols != mat1->cols ||
-      res->cols != mat2->cols)
-  {
-    return;
-  }
-
-  // value addition
-  for (uint32_t i = 0; i < res->rows; i++)
-  {
-    for (uint32_t j = 0; j < res->cols; j++)
-    {
-      xMatrix_set(res, i, j, xMatrix_get(mat1, i, j) + xMatrix_get(mat2, i, j));
+    // pointer checking
+    if (res == NULL || mat1 == NULL || mat2 == NULL) {
+        return;
     }
-  }
 
-  return;
+    // dimension checking
+    if (res->rows != mat1->rows || res->rows != mat2->rows || res->cols != mat1->cols || res->cols != mat2->cols) {
+        return;
+    }
+
+    // value addition
+    for (uint32_t i = 0; i < res->rows; i++) {
+        for (uint32_t j = 0; j < res->cols; j++) {
+            xMatrix_set(res, i, j, xMatrix_get(mat1, i, j) + xMatrix_get(mat2, i, j));
+        }
+    }
+
+    return;
 }
 
 void xMatrix_sub(xMatrix *res, xMatrix *mat1, xMatrix *mat2)
 {
-  // pointer checking
-  if (res == NULL || mat1 == NULL || mat2 == NULL)
-  {
-    return;
-  }
-
-  // dimension checking
-  if (res->rows != mat1->rows || res->rows != mat2->rows || res->cols != mat1->cols ||
-      res->cols != mat2->cols)
-  {
-    return;
-  }
-
-  // value subtraction
-  for (uint32_t i = 0; i < res->rows; i++)
-  {
-    for (uint32_t j = 0; j < res->cols; j++)
-    {
-      xMatrix_set(res, i, j, xMatrix_get(mat1, i, j) - xMatrix_get(mat2, i, j));
+    // pointer checking
+    if (res == NULL || mat1 == NULL || mat2 == NULL) {
+        return;
     }
-  }
 
-  return;
+    // dimension checking
+    if (res->rows != mat1->rows || res->rows != mat2->rows || res->cols != mat1->cols || res->cols != mat2->cols) {
+        return;
+    }
+
+    // value subtraction
+    for (uint32_t i = 0; i < res->rows; i++) {
+        for (uint32_t j = 0; j < res->cols; j++) {
+            xMatrix_set(res, i, j, xMatrix_get(mat1, i, j) - xMatrix_get(mat2, i, j));
+        }
+    }
+
+    return;
 }
 
 void xMatrix_dot(xMatrix *res, xMatrix *mat1, xMatrix *mat2)
 {
-  // pointer checking
-  if (res == NULL || mat1 == NULL || mat2 == NULL || res == mat1 || res == mat2)
-  {
-    return;
-  }
-
-  // dimension checking
-  if (res->rows != mat1->rows || res->cols != mat2->cols || mat1->cols != mat2->rows)
-  {
-    return;
-  }
-
-  // matrix multiplication
-  for (uint32_t i = 0; i < res->rows; i++)
-  {
-    for (uint32_t j = 0; j < res->cols; j++)
-    {
-      float sum = 0.0f;
-      for (uint32_t k = 0; k < mat1->cols; k++)
-      {
-        sum += xMatrix_get(mat1, i, k) * xMatrix_get(mat2, k, j);
-      }
-      xMatrix_set(res, i, j, sum);
+    // pointer checking
+    if (res == NULL || mat1 == NULL || mat2 == NULL || res == mat1 || res == mat2) {
+        return;
     }
-  }
 
-  return;
+    // dimension checking
+    if (res->rows != mat1->rows || res->cols != mat2->cols || mat1->cols != mat2->rows) {
+        return;
+    }
+
+    // matrix multiplication
+    for (uint32_t i = 0; i < res->rows; i++) {
+        for (uint32_t j = 0; j < res->cols; j++) {
+            float sum = 0.0f;
+            for (uint32_t k = 0; k < mat1->cols; k++) {
+                sum += xMatrix_get(mat1, i, k) * xMatrix_get(mat2, k, j);
+            }
+            xMatrix_set(res, i, j, sum);
+        }
+    }
+
+    return;
 }
 
 void xMatrix_transpose(xMatrix *mat)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
-    return;
-  }
-
-  // temporary matrix allocation
-  xMatrix *temp = xMatrix_new(mat->cols, mat->rows);
-  if (temp == NULL)
-  {
-    return;
-  }
-
-  // value copy
-  for (uint32_t i = 0; i < mat->rows; i++)
-  {
-    for (uint32_t j = 0; j < mat->cols; j++)
-    {
-      xMatrix_set(temp, j, i, xMatrix_get(mat, i, j));
+    // pointer checking
+    if (mat == NULL) {
+        return;
     }
-  }
 
-  // matrix swap
-  free(mat->data);
-  mat->data = temp->data;
-  mat->rows = temp->rows;
-  mat->cols = temp->cols;
-  free(temp);
+    // temporary matrix allocation
+    xMatrix *temp = xMatrix_new(mat->cols, mat->rows);
+    if (temp == NULL) {
+        return;
+    }
 
-  return;
+    // value copy
+    for (uint32_t i = 0; i < mat->rows; i++) {
+        for (uint32_t j = 0; j < mat->cols; j++) {
+            xMatrix_set(temp, j, i, xMatrix_get(mat, i, j));
+        }
+    }
+
+    // matrix swap
+    free(mat->data);
+    mat->data = temp->data;
+    mat->rows = temp->rows;
+    mat->cols = temp->cols;
+    free(temp);
+
+    return;
 }
 
 void xMatrix_scale(xMatrix *mat, float scale)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
-    return;
-  }
-
-  // value scaling
-  for (uint32_t i = 0; i < mat->rows; i++)
-  {
-    for (uint32_t j = 0; j < mat->cols; j++)
-    {
-      xMatrix_set(mat, i, j, xMatrix_get(mat, i, j) * scale);
+    // pointer checking
+    if (mat == NULL) {
+        return;
     }
-  }
+
+    // value scaling
+    for (uint32_t i = 0; i < mat->rows; i++) {
+        for (uint32_t j = 0; j < mat->cols; j++) {
+            xMatrix_set(mat, i, j, xMatrix_get(mat, i, j) * scale);
+        }
+    }
 }
 
 void xMatrix_rowAdd(xMatrix *mat, uint32_t row1, uint32_t row2, float scale)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
+    // pointer checking
+    if (mat == NULL) {
+        return;
+    }
+
+    // index checking
+    if (row1 >= mat->rows || row2 >= mat->rows) {
+        return;
+    }
+
+    // value addition
+    for (uint32_t i = 0; i < mat->cols; i++) {
+        xMatrix_set(mat, row1, i, xMatrix_get(mat, row1, i) + xMatrix_get(mat, row2, i) * scale);
+    }
+
     return;
-  }
-
-  // index checking
-  if (row1 >= mat->rows || row2 >= mat->rows)
-  {
-    return;
-  }
-
-  // value addition
-  for (uint32_t i = 0; i < mat->cols; i++)
-  {
-    xMatrix_set(mat, row1, i, xMatrix_get(mat, row1, i) + xMatrix_get(mat, row2, i) * scale);
-  }
-
-  return;
 }
 
 void xMatrix_colAdd(xMatrix *mat, uint32_t col1, uint32_t col2, float scale)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
+    // pointer checking
+    if (mat == NULL) {
+        return;
+    }
+
+    // index checking
+    if (col1 >= mat->cols || col2 >= mat->cols) {
+        return;
+    }
+
+    // value addition
+    for (uint32_t i = 0; i < mat->rows; i++) {
+        xMatrix_set(mat, i, col1, xMatrix_get(mat, i, col1) + xMatrix_get(mat, i, col2) * scale);
+    }
+
     return;
-  }
-
-  // index checking
-  if (col1 >= mat->cols || col2 >= mat->cols)
-  {
-    return;
-  }
-
-  // value addition
-  for (uint32_t i = 0; i < mat->rows; i++)
-  {
-    xMatrix_set(mat, i, col1, xMatrix_get(mat, i, col1) + xMatrix_get(mat, i, col2) * scale);
-  }
-
-  return;
 }
 
 void xMatrix_rowScale(xMatrix *mat, uint32_t rowIndex, float scale)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
+    // pointer checking
+    if (mat == NULL) {
+        return;
+    }
+
+    // index checking
+    if (rowIndex >= mat->rows) {
+        return;
+    }
+
+    // value scaling
+    for (uint32_t i = 0; i < mat->cols; i++) {
+        xMatrix_set(mat, rowIndex, i, xMatrix_get(mat, rowIndex, i) * scale);
+    }
+
     return;
-  }
-
-  // index checking
-  if (rowIndex >= mat->rows)
-  {
-    return;
-  }
-
-  // value scaling
-  for (uint32_t i = 0; i < mat->cols; i++)
-  {
-    xMatrix_set(mat, rowIndex, i, xMatrix_get(mat, rowIndex, i) * scale);
-  }
-
-  return;
 }
 
 void xMatrix_colScale(xMatrix *mat, uint32_t colIndex, float scale)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
+    // pointer checking
+    if (mat == NULL) {
+        return;
+    }
+
+    // index checking
+    if (colIndex >= mat->cols) {
+        return;
+    }
+
+    // value scaling
+    for (uint32_t i = 0; i < mat->rows; i++) {
+        xMatrix_set(mat, i, colIndex, xMatrix_get(mat, i, colIndex) * scale);
+    }
+
     return;
-  }
-
-  // index checking
-  if (colIndex >= mat->cols)
-  {
-    return;
-  }
-
-  // value scaling
-  for (uint32_t i = 0; i < mat->rows; i++)
-  {
-    xMatrix_set(mat, i, colIndex, xMatrix_get(mat, i, colIndex) * scale);
-  }
-
-  return;
 }
 
 float xMatrix_get(xMatrix *mat, uint32_t rowIndex, uint32_t colIndex)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
-    return 0.0f;
-  }
+    // pointer checking
+    if (mat == NULL) {
+        return 0.0f;
+    }
 
-  // index checking
-  if (rowIndex >= mat->rows || colIndex >= mat->cols)
-  {
-    return 0.0f;
-  }
+    // index checking
+    if (rowIndex >= mat->rows || colIndex >= mat->cols) {
+        return 0.0f;
+    }
 
-  // value retrieval
-  return mat->data[rowIndex * mat->cols + colIndex];
+    // value retrieval
+    return mat->data[rowIndex * mat->cols + colIndex];
 }
 
 void xMatrix_set(xMatrix *mat, uint32_t rowIndex, uint32_t colIndex, float value)
 {
-  // pointer checking
-  if (mat == NULL)
-  {
+    // pointer checking
+    if (mat == NULL) {
+        return;
+    }
+
+    // index checking
+    if (rowIndex >= mat->rows || colIndex >= mat->cols) {
+        return;
+    }
+
+    // value setting
+    mat->data[rowIndex * mat->cols + colIndex] = value;
+
     return;
-  }
-
-  // index checking
-  if (rowIndex >= mat->rows || colIndex >= mat->cols)
-  {
-    return;
-  }
-
-  // value setting
-  mat->data[rowIndex * mat->cols + colIndex] = value;
-
-  return;
 }
