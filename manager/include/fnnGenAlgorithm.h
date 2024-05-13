6a2db771862e64f633292ecc64c037536dd9157c
--get <hogar1.veliki@gmail.com>
2024-05-13 02:46:46 +0200

    Added function for creating new model from two parent ones

--- manager/include/fnnGenAlgorithm.h
+++ manager/include/fnnGenAlgorithm.h
@@ -1,8 +1,7 @@
 /**
  * @file fnnGenAlgorithm.h
  * @author 0xDontCare (https://github.com/0xDontCare)
- * @brief Genetic algorithm functions for feedforward neural network (FNN)
- * weights and biases
+ * @brief Genetic algorithm functions for feedforward neural network (FNN) weights and biases
  * @version 0.1
  * @date 08.05.2024.
  *
@@ -19,18 +18,59 @@ extern "C" {
 
 #include <stdint.h>
 
+#include "fnnSerializer.h"
+
 /**
- * @brief Generate random weights for the FNN based on given layer neurons and
- * range
- * @details The weights are generated as uniformly distributed random numbers in
- * the given range.
+ * @brief Generate randomized feedforward neural network model based on given parameters
+ *
+ * @param neuronCounts Number of neurons in each layer
+ * @param activationFunctions Activation functions for each layer (except the input layer)
+ * @param layerCount Total number of layers in the FNN
+ * @param weightMin Minimum value weight can have
+ * @param weightMax Maximum value weight can have
+ * @param biasMean Mean value for the biases
+ * @param biasStddev Standard deviation for the biases
+ * @return Pointer to generated FNN model descriptor or NULL if the generation failed
+ *
+ * @note The caller is responsible for freeing the memory.
+ *
+ * @note Weights are generated as uniformly distributed random numbers in the given range.
+ *
+ * @note Biases are generated as normally distributed random numbers with set mean and standard deviation.
+ */
+FnnModel *fnn_generateModel(uint32_t *neuronCounts, FnnActivation_e *activationFunctions, uint32_t layerCount, float weightMin,
+                            float weightMax, float biasMean, float biasStddev);
+
+/**
+ * @brief Breed two feedforward neural network models to create a new one
+ *
+ * @param parent1 Pointer to the first parent FNN model
+ * @param parent2 Pointer to the second parent FNN model
+ * @param sbxCrossDistrIndex Distribution index for the crossover (probability value between 0 and positive infinity)
+ * @param mutationRate Rate of mutation (probability value between 0 and 1) for each weight and bias
+ * @param mutationStddev Standard deviation for the mutation
+ * @return Pointer to descriptor of the new FNN model or NULL if process failed
+ *
+ * @note The caller is responsible for freeing the memory.
+ *
+ * @warning Both parent FNN models must have same total number of weights and biases.
+ *
+ * @note The crossover is done using simulated binary crossover (SBX) algorithm.
+ *
+ * @note The mutation is done using normal distribution around the current value.
+ */
+FnnModel *fnn_modelBreed(FnnModel *parent1, FnnModel *parent2, float sbxCrossDistrIndex, float mutationRate,
+                            float mutationStddev);
+
+/**
+ * @brief Generate random weights for the FNN based on given layer neurons and range
+ * @details The weights are generated as uniformly distributed random numbers in the given range.
  *
  * @param layerNeurons Number of neurons in each layer
  * @param layerCount Number of layers in the FNN
  * @param rangeMin Minimum value for the weights
  * @param rangeMax Maximum value for the weights
- * @return `float*`: Pointer to the generated weights or NULL if the generation
- * failed
+ * @return `float*`: Pointer to the generated weights or NULL if the generation failed
  *
  * @note The caller is responsible for freeing the memory.
  *
@@ -38,7 +78,7 @@ extern "C" {
  *
  * @warning There needs to be at least 2 layers for the FNN to be valid.
  */
-float *fnn_generateWeights(uint32_t *layerNeurons, uint32_t layerCount, float rangeMin, float rangeMax);
+float *fnn_generateWeights(const uint32_t *layerNeurons, uint32_t layerCount, float rangeMin, float rangeMax);
 
 /**
  * @brief Generate random biases for the FNN based on given layer neurons
@@ -49,14 +89,13 @@ float *fnn_generateWeights(uint32_t *layerNeurons, uint32_t layerCount, float ra
  * @param layerCount Number of layers in the FNN
  * @param mean Mean value for the biases
  * @param stddev Standard deviation for the biases
- * @return `float*`: Pointer to the generated biases or NULL if the generation
- * failed
+ * @return `float*`: Pointer to the generated biases or NULL if the generation failed
  *
  * @note The caller is responsible for freeing the memory.
  *
  * @warning There needs to be at least 2 layers for the FNN to be valid.
  */
-float *fnn_generateBiases(uint32_t *layerNeurons, uint32_t layerCount, float mean, float stddev);
+float *fnn_generateBiases(const uint32_t *layerNeurons, uint32_t layerCount, float mean, float stddev);
 
 /**
  * @brief Crossover operator for two parent arrays
@@ -64,21 +103,16 @@ float *fnn_generateBiases(uint32_t *layerNeurons, uint32_t layerCount, float mea
  * @param parent1 Pointer to the first parent array
  * @param parent2 Pointer to the second parent array
  * @param numElements Number of elements in the parent arrays
- * @param distributionIndex Distribution index for the crossover (probability
- * value between 0 and 1)
- * @return `float*`: Pointer to the new array with the crossover result or NULL
- * if the crossover
+ * @param distributionIndex Distribution index for the crossover (probability value between 0 and positive infinity)
+ * @return `float*`: Pointer to the new array with the crossover result or NULL if the crossover
  *
- * @note The caller is responsible for freeing the memory if parent arrays are
- * not needed anymore.
+ * @note The caller is responsible for freeing the memory if parent arrays are not needed anymore.
  *
- * @warning `layerNeurons` and `layerCount` must be the same for both parent
- * FNNs.
+ * @warning `layerNeurons` and `layerCount` must be the same for both parent FNNs.
  *
  * @note The crossover is done using simulated binary crossover (SBX) algorithm.
- *
  */
-float *fnn_crossover(float *parent1, float *parent2, uint64_t numElements, float distributionIndex);
+float *fnn_crossover(const float *parent1, const float *parent2, uint64_t numElements, float distributionIndex);
 
 /**
  * @brief Mutate the weights of the FNN
@@ -88,8 +122,7 @@ float *fnn_crossover(float *parent1, float *parent2, uint64_t numElements, float
  * @param mutationRate Rate of mutation (probability value between 0 and 1)
  * @param stddev Standard deviation for the mutation
  *
- * @note The mutation is done using normal distribution around the current
- * value.
+ * @note The mutation is done using normal distribution around the current value.
  */
 void fnn_mutate(float *values, uint64_t numElements, float mutationRate, float stddev);
 
--- manager/src/fnnGenAlgorithm.c
+++ manager/src/fnnGenAlgorithm.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "fnnSerializer.h"
+
 /**
  * @brief Generate random number from normal distribution
  *
@@ -58,7 +60,7 @@ static float distanceFactor(float distrIndex)
 
     // calculate the distance factor
     return (random > 0.5f) ? powf(2.0f * random, 1.0f / (distrIndex + 1.0f))
-                          : 1.0f / powf(2.0f * (1.0f - random), 1.0f / (distrIndex + 1.0f));
+                           : 1.0f / powf(2.0f * (1.0f - random), 1.0f / (distrIndex + 1.0f));
 }
 
 /**
@@ -104,7 +106,105 @@ static void fnn_fillNormal(float *array, uint64_t arrLen, float mean, float stdD
     }
 }
 
-float *fnn_generateWeights(uint32_t *layerNeurons, uint32_t layerCount, float rangeMin, float rangeMax)
+FnnModel *fnn_generateModel(uint32_t *neuronCounts, FnnActivation_e *activationFunctions, uint32_t layerCount, float weightMin,
+                            float weightMax, float biasMean, float biasStddev)
+{
+    // parameter checking
+    if (neuronCounts == NULL || activationFunctions == NULL || layerCount < 2 || weightMin > weightMax || biasStddev < 0) {
+        return NULL;
+    }
+
+    // memory allocation
+    FnnModel *model = fnn_new();
+
+    // generate weights and biases
+    float *weights = fnn_generateWeights(neuronCounts, layerCount, weightMin, weightMax);
+    float *biases = fnn_generateBiases(neuronCounts, layerCount, biasMean, biasStddev);
+
+    // count up total weights and biases
+    for (uint32_t i = 0; i < layerCount - 1; i++) {
+        model->totalWeights += neuronCounts[i] * neuronCounts[i + 1];
+        model->totalBiases += neuronCounts[i + 1];
+    }
+
+    // assign values to the model
+    model->layerCount = layerCount;
+    model->neuronCounts = neuronCounts;
+    model->activationFunctions = activationFunctions;
+    model->weightValues = weights;
+    model->biasValues = biases;
+
+    return model;
+}
+
+FnnModel *fnn_modelBreed(FnnModel *parent1, FnnModel *parent2, float sbxCrossDistrIndex, float mutationRate, float mutationStddev)
+{
+    // parameter checking
+    if (parent1 == NULL || parent2 == NULL || sbxCrossDistrIndex < 0 || mutationRate < 0 || mutationRate > 1 ||
+        mutationStddev < 0 || parent1->totalWeights != parent2->totalWeights || parent1->totalBiases != parent2->totalBiases) {
+        return NULL;
+    }
+
+    // memory allocation
+    FnnModel *child = fnn_new();
+    if (child == NULL) {
+        return NULL;
+    }
+
+    // crossover
+    if (parent1->weightValues != NULL && parent2->weightValues != NULL) {
+        float *childWeights = fnn_crossover(parent1->weightValues, parent2->weightValues, parent1->totalWeights, sbxCrossDistrIndex);
+        if (childWeights == NULL) {
+            fnn_free(child);
+            return NULL;
+        }
+        child->weightValues = childWeights;
+    }
+
+    if (parent1->biasValues != NULL && parent2->biasValues != NULL) {
+        float *childBiases = fnn_crossover(parent1->biasValues, parent2->biasValues, parent1->totalBiases, sbxCrossDistrIndex);
+        if (childBiases == NULL) {
+            fnn_free(child);
+            return NULL;
+        }
+        child->biasValues = childBiases;
+    }
+
+    // mutation
+    if (child->weightValues != NULL) {
+        fnn_mutate(child->weightValues, parent1->totalWeights, mutationRate, mutationStddev);
+    }
+
+    if (child->biasValues != NULL) {
+        fnn_mutate(child->biasValues, parent1->totalBiases, mutationRate, mutationStddev);
+    }
+
+    // assign values to the model
+    child->layerCount = parent1->layerCount;
+    child->totalWeights = parent1->totalWeights;
+    child->totalBiases = parent1->totalBiases;
+    
+    child->neuronCounts = (uint32_t *)malloc(sizeof(uint32_t) * parent1->layerCount);
+    if (child->neuronCounts == NULL) {
+        fnn_free(child);
+        return NULL;
+    }
+    for (uint32_t i = 0; i < parent1->layerCount; i++) {
+        child->neuronCounts[i] = parent1->neuronCounts[i];
+    }
+    child->activationFunctions = (FnnActivation_e *)malloc(sizeof(FnnActivation_e) * (parent1->layerCount - 1));
+    if (child->activationFunctions == NULL) {
+        fnn_free(child);
+        return NULL;
+    }
+    for (uint32_t i = 0; i < parent1->layerCount - 1; i++) {
+        child->activationFunctions[i] = parent1->activationFunctions[i];
+    }
+
+    return child;
+}
+
+float *fnn_generateWeights(const uint32_t *layerNeurons, uint32_t layerCount, float rangeMin, float rangeMax)
 {
     // parameter checking
     if (layerNeurons == NULL || layerCount < 2 || rangeMin > rangeMax) {
@@ -127,7 +227,7 @@ float *fnn_generateWeights(uint32_t *layerNeurons, uint32_t layerCount, float ra
     return weights;
 }
 
-float *fnn_generateBiases(uint32_t *layerNeurons, uint32_t layerCount, float mean, float stddev)
+float *fnn_generateBiases(const uint32_t *layerNeurons, uint32_t layerCount, float mean, float stddev)
 {
     // parameter checking
     if (layerNeurons == NULL || layerCount < 2 || stddev < 0) {
@@ -150,7 +250,7 @@ float *fnn_generateBiases(uint32_t *layerNeurons, uint32_t layerCount, float mea
     return biases;
 }
 
-float *fnn_crossover(float *parent1, float *parent2, uint64_t numElements, float distributionIndex)
+float *fnn_crossover(const float *parent1, const float *parent2, uint64_t numElements, float distributionIndex)
 {
     // parameter checking
     if (parent1 == NULL || parent2 == NULL || numElements == 0 || distributionIndex < 0) {
